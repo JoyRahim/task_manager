e9eafb9dcde231982ee1b2cb42d7ae7b869ac9d3
rahimjoy <md.rahimjoy@gmail.com>
2025-10-22 20:24:48 +0600

    first commit

---
+++ lib/ui/utils/asset_paths.dart
@@ -0,0 +1,7 @@
+class AssetPaths {
+  static const String _imagePath = 'assets/images';
+
+  static const String backgroundSvg = '$_imagePath/background.svg';
+  static const String logoSvg = '$_imagePath/logo.svg';
+  static const String penSvg = '$_imagePath/pen.svg';
+}
